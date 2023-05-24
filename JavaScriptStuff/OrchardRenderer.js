46ee762cfec4844dee37d022ea3eca0de0a9fbd6
Hayes-Torrence <68446206+Hayes-Torrence@users.noreply.github.com>
2023-05-24 13:01:19 -0400

    Processing Rows
    Started implementation of multiple rows, and multiple trees with the same plant date.
    
    To handle this implemented processRow method, which takes 
    a row, and loads it into an array of arrays. The first entry of each sub array is the "plant date" with all following entries being trees with the appropriate plant date. 
    
    In future this structure may become more deeply ingrained into the shape of a Row object, but for now it exists as a way to take the loosely organized row structure, and convert it into one more suited for rendering purposes.

--- JavaScriptStuff/OrchardRenderer.js
+++ JavaScriptStuff/OrchardRenderer.js
@@ -38,8 +38,12 @@
 			var raycaster = new THREE.Raycaster();
 			var pointer = new THREE.Vector2();
 			var intersects;
-			const orchard = [];//array of Rows (for now)
 
+			var orchard = Orchard("test");
+
+
+			//define two camera arangements: one for horizontal, one for vertical,
+			//when flip between the two just jump between those two camera orientations.
 
 
 
@@ -84,17 +88,17 @@
 
 
 				//Make a new geometry, add it to the scene
-/*
-				addTreeTest(-10);
-				addTreeTest(4);
-				addTreeTest(10);
-*/
+
 				var testRow3 = Row("The Ottomans",-10, 10);
 				var testTree = Tree("Sulyman", 9);
 				var testLeaf = Leaf("Sulyman executed the grandvizir", 7);
+				
+				orchard.addRow_(testRow3);
 
 				var testTree2 = Tree("Osman", -10);
 				var testLeaf2 = Leaf("Osman entered the singularity and emerged unscathed", 1);
+				testTree2.AddLeaf_(Leaf("Armed with knowledge gleaned from the time snakes, osman set out to forge his new empire"), 3);
+				testRow3.AddTree_(Tree("Mehmed", 1));
 
 
 				testRow3.AddTree_(testTree);
@@ -104,11 +108,14 @@
 				testRow3.AddTree_(testTree2);
 				testTree2.AddLeaf_(testLeaf2);
 
-				drawRow(testRow3);
+				testRow3.AddTree_(Tree("TimeSnake Osman", 1));
+
+
+				//drawRow(testRow3);
 
 				console.log(testRow3);
 				console.log(testRow3.trees_[0]);
-				console.log(orchard[0].name);
+				console.log(orchard.name);
 				
 
 				drawConnection(testTree, testTree2);
@@ -118,7 +125,10 @@
 				renderer.setSize( window.innerWidth, window.innerHeight );
 				document.body.appendChild( renderer.domElement );
 
-				//
+				drawRow(testRow3);
+
+				processRow(testRow3);
+				
 
 				stats = new Stats();
 				document.body.appendChild( stats.dom );
@@ -126,7 +136,11 @@
 				//
 
 				const gui = new GUI();
-				//gui.add( material, 'wireframe' );
+				gui.add(orchard, 'horizontal_').onChange( value =>{
+					console.log("onChange");
+					ChangeView();
+				});
+				
 
 				//
 
@@ -147,6 +161,103 @@
 				pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 				pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
+			}
+
+
+			function drawOrchard(){
+				//Boot strapper that starts and handles the whole process of drawing an orchard:
+					//currently just want to be able to display multiple rows
+
+
+				//processRow's
+				//use the returned array to figure some stuff out:
+					//whats the date with the most nodes present: that's our max width
+
+
+				//use the new row objects allong with the determined info to draw out each node
+
+			}
+
+			function processRow(row){
+				//Version 1 implementation -> messy
+
+				//takes a row of trees, loads it into an array, 
+
+				const rowP = [];
+				let currentPlantDate; //TODO: better varaible name
+
+
+				console.log("row.trees_[0].plantDate_");
+				console.log(row.trees_[0].plantDate_);
+
+				rowP.push([row.trees_[0].plantDate_, row.trees_[0]]);
+				//not sure how efficient splice is as an opperation,
+				//but efficiency is a concern for the next draft
+
+				for(let i = 1 ; i < row.trees_.length ; i++){
+					//itterates across all the trees in a row
+
+
+
+					currentPlantDate = row.trees_[i].plantDate_; //
+
+
+					/*
+					console.log("********");
+					console.log("i loop: " + i);
+					console.log("row.trees_[i].plantDate_");
+					console.log(row.trees_[i].plantDate_);
+					console.log("________________");
+					console.log("rowP.length: ");
+					console.log(rowP.length);
+					console.log("--------");
+					console.log("currentPlantDate: ");
+					console.log(currentPlantDate);
+					*/
+
+					for(let j = 0 ; j < rowP.length ; j++){
+						//itterates through rowP to find where to insert element,
+						//once it finds a duplicate plant date, or one where the next is greater,
+						//and the current is lesser, sticks it there
+
+						console.log("j loop: " + j);
+
+						if(j > 6){
+							//TODO: remove this once confident in safety of code
+							break;
+							//breaks to make sure I don't accidentally infinite loop again
+						}
+
+						if(rowP[j][0] === currentPlantDate){
+							//
+							console.log("firstONe");
+							console.log(row.trees_[i]);
+							console.log("%%%%%%%%%");
+							rowP[j].push(row.trees_[i]);
+							break; 
+						}
+						else if(rowP[j][0] > currentPlantDate){
+							//if we find a plant date bigger than the current we stick the current one
+							//before that
+							console.log("secondOne");
+							console.log(row.trees_[i]);
+							console.log("%%%%%%%%%");
+							rowP.splice(j, 0 , [row.trees_[i].plantDate_, row.trees_[i]]);
+							break;
+
+						}
+						else if(j === rowP.length-1){
+							//if we're on the last index, and no other input breaks first,
+							//sticks this entry on to the end of the list
+							rowP.push([row.trees_[i].plantDate_, row.trees_[i]]);
+							break;
+						}
+					}
+				}
+
+				//so now we have a 2d array where the first entry is the plant date,
+				//and all the subsequent values are trees planted at that date
+				return rowP;
 			}
 
 
@@ -182,6 +293,24 @@
 
 			}
 
+			function addTreeTest(tree){
+				//adds tree object to scene
+				//adds pointer to underlying tree object for use in displaying info
+
+				const treeGeometry = new THREE.BoxGeometry( 2, 2, 1 ); 
+   				const treeMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+    			const cube = new THREE.Mesh( treeGeometry, treeMaterial ); 
+				cube.translateOnAxis(new THREE.Vector3(0,1,0).normalize(), tree.plantDate_);
+				cube.tree_ = tree;
+				//TODO: figure if this is passing pointers (if js even does that)
+				//and if it's not see if can improve memory efficiency
+    			scene.add( cube );
+			}
+
+
+
+
+
 			function drawConnection(Tree1, Tree2){
 				//method that draws a connecting line between two trees
 
@@ -190,7 +319,7 @@
 				const points = [];
 
 				//points.push(new THREE.Vector3())
-				//feels like I need more info: need to store the specific location of each tree?
+
 				//Maybe a method within row that can quickly convert the plant date over into
 				//an appropriate vector
 				points.push(new THREE.Vector3(0, Tree1.plantDate_, 0));
@@ -220,22 +349,7 @@
 
 
 
-			function addTreeTest(tree){
-				//adds tree object to scene
 
-
-				//should also add some sort of pointer to the mesh that points to the 
-				//underlying tree object so that the intersect method can grab info from there
-
-				const treeGeometry = new THREE.BoxGeometry( 2, 2, 1 ); 
-   				const treeMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
-    			const cube = new THREE.Mesh( treeGeometry, treeMaterial ); 
-				cube.translateOnAxis(new THREE.Vector3(0,1,0).normalize(), tree.plantDate_);
-				cube.tree_ = tree;
-				//TODO: figure if this is passing pointers (if js even does that)
-				//and if it's not see if can improve memory efficiency
-    			scene.add( cube );
-			}
 
 
 		
@@ -249,23 +363,75 @@
 
 				const orch = {
 					name_: name,
-					orientation_: 1,
+					horizontal_: false, 
 					rows_: [],
+					connections_: [],
 					addRow_: function(row){
-						rows_.push(row);
+						this.rows_.push(row);
+					},
+					isHorizontal_: function(){
+						if(this.horizontal_ === true){
+							return true;
+						}
+						return false;
+					},
+					isVertical_: function(){
+						//artifact of previous "orientation_" implementation
+						if(this.horizontal_ === false){
+							return true;
+						}
+						return false;
 					}
+
 				}
 				return orch;
 			}
+
+			function HorizontalView(){
+
+				//This implementation vs actually changing the coordinates could have some
+				//unplesant effects once I'm actually rending images and text in the thing.
+				//but for now is fast and effective
+
+				//rotates the camera 90 degrees and sets the boolean flags in orchard appropriatly
+				if(orchard.isVertical_){
+					camera.rotateOnAxis(new THREE.Vector3(0,0,1), Math.PI/2 );
+					orchard.horizontal_ = true;
+				}
+			}
+
+			function ChangeView(){
+				//rotates the camera 90 degrees and sets the boolean flags in orchard appropriatly
+
+				//TODO: Currently only effects the value of orchard.horizontal_ in the local scope?
+				//somehow?
+				//So instead of jumping back and forth between two states, we rotate in 1/4 circle 
+				//increments. Need to determine why this is happening, and stop it	
+
+				console.log("var: " + orchard.horizontal_);
+
+				if(orchard.horizontal_ === true){
+					console.log("if");
+					camera.rotateOnAxis(new THREE.Vector3(0,0,1), Math.PI/2 );
+					orchard.horizontal_ = false;
+				}
+				if(!orchard.horizontal_ === false){
+					console.log("else");
+					camera.rotateOnAxis(new THREE.Vector3(0,0,1), -Math.PI/2 );
+					orchard.horizontal_ = true;
+				}
+
+				console.log("orchard: " + orchard.horizontal_);
+				console.log("----------------------------------")
+			}
+
 
 
 
 
 			function Row(nameStr, start, end){
 				//Creates a new row and adds it to the Orchard Array
-
 				//dummy variables start and end
-
 
 				const row = {
 					start_: start,
@@ -275,16 +441,17 @@
 					AddTree_: function(tree){
 						this.trees_.push(tree);
 					}
-				}
-				orchard.push(row);
+				}			
 				return row;
-
 			}
 
 
 			function Tree(nameStr, PlantDate){
 				//creates a new Tree Object
 				//contians a content String, and a visible date
+				//what if this was just a 3Js object,
+				//and we added additional fields on?
+				
 
 				const tree = {
 					name_: nameStr,
@@ -307,6 +474,46 @@
 				return leaf;
 			}
 		
+			function Connection(Leaf1, Leaf2, Content){
+				//in my head, a row is too ordered, it implies some sort of heirarchy,
+				//but I think in reality the implementaion I'm going for is far less ordered,
+				//what I really want a row to contain is a list of tree's
+
+
+				//These connections are meant to pull out some of the "connected-ness"
+				// a row is assumed to have. All a row is, is a collection of tree's that share
+				//some kind of commonality.
+				//A connection object is a connection between any two tree's:
+				//they can be in the same row, they can be in different rows.
+				//TODO: should connections exist on a tree level or a leaf level?
+				//I think they should exist as tree to tree conections that can interface with leafs.
+
+				//a connection is just a statement that two trees are connected.
+				//future implementations could include the ability to define, mid use
+				//new kinds of connection: black means heriditary, dotted means divorce, etc.
+				//connections should be effected by the view window.
+
+				//connections need to be housed at the orchard level to allow them interact w/ different
+				//rows.
+
+
+				//TODO: determine how to implement visible date:
+				//If connections are between tree's but the actual info that creates a connection
+				//lives on the leaf level, then should probably just pull from the leaf's visible date
+				//that being said, I don't believe a leaf has any way of pointing at the 
+				//tree it lives on, so it might get messy to just hand it leaves.
+				//of course that could be fixed by giving leaves a pointer towards the parent tree,
+				//but then things become more circualar, and I have to ask if theres a better way.
+				const connec = {
+					leaf1_: Leaf1,
+					leaf2_: Leaf2,
+					content_: Content
+				}
+
+
+				return connec;
+
+			}
 
 
 
@@ -322,14 +529,28 @@
 
 			}
 
-			//
-
 			function animate() {
 
 				requestAnimationFrame( animate );
 
+				//Spin();
+				//WEEEEEEEEEEEEEEEEEEEEEEEW
+
 				render();
+			
 				stats.update();
+
+			}
+
+
+			function Spin(){
+				//rotates the camera 90 degrees and sets the boolean flags in orchard appropriatly
+				if(orchard.isVertical_){
+					
+					camera.rotateOnAxis(new THREE.Vector3(0,0,1), .01 );
+
+				}
+
 
 			}
 
